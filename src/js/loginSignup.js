f783eafc3e9a1c1469cb64442c5b0070d82328fc
Michael James <micrjames921@gmail.com>
2022-10-18 11:42:27 -0700

    fixed issues with scss code; improved js and scss behavior to improve look and feel of layout

--- src/js/loginSignup.js
+++ src/js/loginSignup.js
@@ -3,10 +3,14 @@ import { lightestBgColor, lighterBgColor } from "./incs.js";
 const toggleFormState = function(startState, endState) {
    if(startState[1].classList.contains("hidden")) startState[1].classList.remove("hidden");
    startState[0].style.backgroundColor = lightestBgColor;
-   startState[0].style.borderBottom = "none";
+   startState[0].style.borderTopStyle = "solid";
+   startState[0].style.borderRightStyle = "solid";
+   startState[0].style.borderLeftStyle = "solid";
 
-   endState[0].style.backgroundColor = lighterBgColor;
    endState[1].classList.add("hidden");
+   endState[0].style.backgroundColor = lighterBgColor;
+   endState[0].style.borderTopStyle = "dotted";
+   endState[0].style.borderRightStyle = "dotted";
 };
 
 export { toggleFormState };
